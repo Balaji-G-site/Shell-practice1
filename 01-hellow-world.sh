b03fda0094541625a8196b96fc829966f06e1303
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-19 16:57:23 +0530

    hellow

---
+++ 01-hellow-world.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Balaji
+PERSON2=Sudha
+
+echo "$1: Hii $2"
+echo "$2: Hii $1"
+echo "$1: HOw are you baby"
+echo "$2: fine dear what about you...!"
+echo "$1: yes, i'm fine, thank you...!"
