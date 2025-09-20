cbb7eba496b59e0a6fa1a00ec817af2a8531a03c
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-20 16:02:25 +0530

    hello world started

--- 03-install.sh
+++ 03-install.sh
@@ -9,3 +9,21 @@ if [ $USERID -ne 0 ]
     else
     echo "You are running with root user"
 fi
+
+dnf list installed mysql
+
+if [ $? -nq 0 ]
+    then
+        echo "Mysql is going to...install"
+        
+        dnf install Mysql
+        if [ $? -eq 0 ]
+        then
+            echo "Mysql installing...Successful"
+        else
+            echo "Mysql installing...Failure"
+            exit 1
+        fi
+    else
+        echo "Mysql is already installed...nothing to do"
+    fi
