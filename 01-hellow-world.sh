163083536d19f56109bdd946f51d3c04451d5fc3
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-20 15:43:11 +0530

    hello world started

--- 01-hellow-world.sh
+++ 01-hellow-world.sh
@@ -3,8 +3,8 @@
 PERSON1=Balaji
 PERSON2=Sudha
 
-echo "$1: Hii $2"
-echo "$2: Hii $1"
-echo "$1: HOw are you baby"
-echo "$2: fine dear what about you...!"
-echo "$1: yes, i'm fine, thank you...!"
+echo "$PERSON1: Hii $PERSON2"
+echo "$PERSON2: Hii $PERSON1"
+echo "$PERSON1: HOw are you baby"
+echo "$PERSON2: fine dear what about you...!"
+echo "$PERSON1: yes, i'm fine, thank you...!"
