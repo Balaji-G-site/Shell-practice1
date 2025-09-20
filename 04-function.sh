426022c141b38abd8aea807e27f8919ce40bb3cc
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-20 16:27:01 +0530

    hello world started

---
+++ 04-function.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if $USERID [ -ne 0 ]
+then
+    echo " ERROR::Please run this script with root user"
+    exit 1
+else
+    echo " You are running with root user"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "installing $2 is...SUCCESSFUL"
+    else
+        echo "installing $2 is...FAILURE"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0]
+then
+    echo "Mysql is not installed...going to install"
+    dnf install mysql
+    VALIDATE $? "Mysql"
+else
+    echo "Mysql is already installed...nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -nq 0 ]
+then 
+    echo "nginx is not installed...going to install"
+    dnf install nginx
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "Python3 not installed...going to install"
+    dnf install python3
+    VALIDATE $? "python3"
+else
+    echo " Python3 is already installed...nothing to do"
+
+
