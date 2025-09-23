3b82c8cc672cadf0b466b066b151f8509a5297b1
Balaji-G-site <gundlapallibalaji@gmail.com>
2025-09-23 15:05:57 +0530

    hello world

---
+++ 05-colors.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e [31m"
+G="\e [32m"
+Y="\e [33m"
+N="\e [0m"
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root user $N"
+    exit 1
+else
+    echo "You are running with root user"
+fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "installing $2 is...$G SUCCESSFUL $N"
+    else
+        echo -e "installing $2 is...$R FAILURE $N"
+        exit 1
+    fi
+}
+
+dnf list installed mysql
+if [ $? -ne o ]
+then
+    echo "mysql is not installed...going to insatll
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo -e "mysql is $Y already installed $N, nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed going to install"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo -e "nginx is $Y already installed $N, nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then 
+    echo "python3 is not installed, going to install"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "python3 is $Y already installed $N, nothing to do"
+fi
